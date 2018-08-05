b1fa5eb15dd9738ffe215afc9da1734617f8737c
Yang Liu <foamliu@yeah.net>
2018-08-05 20:22:38 +0800

    correct test  package names

--- config.py
+++ config.py
@@ -21,11 +21,10 @@ train_image_folder = os.path.join(train_folder, 'keypoint_train_images_20170902'
 valid_image_folder = os.path.join(valid_folder, 'keypoint_validation_images_20170911')
 test_a_image_folder = os.path.join(test_a_folder, 'keypoint_test_a_images_20180103')
 test_b_image_folder = os.path.join(test_b_folder, 'keypoint_test_b_images_20180103')
-train_annotations_filename = 'keypoint_train_annotations_20170909.json'
-valid_annotations_filename = 'keypoint_validation_annotations_20170911.json'
-test_a_annotations_filename = 'keypoint_test_a_annotations_20180103.json'
-test_b_annotations_filename = 'keypoint_test_b_annotations_20180103.json'
-
+train_annotations_filename = os.path.join(train_folder, 'keypoint_train_annotations_20170909.json')
+valid_annotations_filename = os.path.join(valid_folder, 'keypoint_validation_annotations_20170911.json')
+test_a_annotations_filename = os.path.join(test_a_folder, 'keypoint_test_a_annotations_20180103.json')
+test_b_annotations_filename = os.path.join(test_b_folder, 'keypoint_test_b_annotations_20180103.json')
 
 start_word = '<start>'
 stop_word = '<end>'
