cfc89de59cd3e7a388faf0c615052787167ec95b
Yang Liu <foamliu@yeah.net>
2018-08-28 14:41:04 +0800

    update

--- demo.py
+++ demo.py
@@ -16,24 +16,13 @@ if __name__ == '__main__':
 
     test_image = 'images/ski.jpg'
     oriImg = cv.imread(test_image)  # B,G,R order
-    plt.imshow(oriImg[:, :, [2, 1, 0]])
-
-    heatmap_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 15))
-    paf_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 30))
-
     imageToTest = cv.resize(oriImg, (image_h, image_w), interpolation=cv.INTER_CUBIC)
-    plt.imshow(imageToTest[:, :, [2, 1, 0]])
-    plt.show()
 
-    input_img = np.transpose(np.float32(imageToTest[:, :, :, np.newaxis]),
-                             (3, 0, 1, 2))  # required shape (1, width, height, channels)
+    input_img = np.expand_dims(imageToTest, 0)
     input_img = input_img / 256 - 0.5
-    print("Input shape: " + str(input_img.shape))
 
-    batch_paf_masks = np.empty((1, 46, 46, num_connections * 2), dtype=np.uint8)
-    batch_paf_masks[0] = ALL_PAF_MASK
-    batch_heatmap_masks = np.empty((1, 46, 46, num_joints_and_bkg), dtype=np.uint8)
-    batch_heatmap_masks[0] = ALL_HEATMAP_MASK
+    batch_paf_masks = np.expand_dims(ALL_PAF_MASK, 0)
+    batch_heatmap_masks = np.expand_dims(ALL_HEATMAP_MASK, 0)
 
     output_blobs = model.predict([input_img, batch_paf_masks, batch_heatmap_masks])
     print("Output shape (heatmap): " + str(output_blobs[1].shape))
@@ -45,7 +34,7 @@ if __name__ == '__main__':
 
     # visualization
     plt.imshow(imageToTest[:, :, [2, 1, 0]])
-    plt.imshow(heatmap, alpha=.5)  # right elbow
+    plt.imshow(heatmap[:, :, 1], alpha=.5)  # right elbow
     plt.savefig('images/demo.png')
 
     # paf = np.squeeze(output_blobs[0])  # output 0 is PAFs
